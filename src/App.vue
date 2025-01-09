70c0c716033c0600d886b19181d738f962c7b75d
Tornike Nanobashvili <tnanobashvili92@gmail.com>
2025-01-09 15:14:58 +0400

    Refactoring App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,35 @@
 <template>
     <div>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Royal Infinity</a>
+            <div class="container">
+                <!-- Brand -->
+                <a class="navbar-brand me-3" href="#">Golden Empire</a>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <!-- Toggler Button -->
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
+                <!-- Collapsible Menu -->
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item" v-for="route in routes" :key="route.name">
-                            <router-link class="nav-link" :to="route.path">{{ route.name }}</router-link>
+                            <router-link class="nav-link px-3" :to="route.path">{{ route.name }}</router-link>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
 
-        <div class="container mt-4">
+        <div class="container text-center mt-4">
             <router-view></router-view>
         </div>
     </div>
