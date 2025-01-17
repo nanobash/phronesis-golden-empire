b089851109daae666300014ddd5b914616077d94
Tornike Nanobashvili <tnanobashvili92@gmail.com>
2025-01-17 16:01:49 +0400

    Rename page to cashier

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,12 +1,12 @@
 // src/router.js
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from './pages/HomePage.vue';
-import Project21 from './pages/Project21.vue';
+import Cashier from './pages/Cashier.vue';
 import DebriefPage from './pages/DebriefPage.vue';
 
 const routes = [
     { path: '/', component: HomePage, name: 'Home' },
-    { path: '/project21', component: Project21, name: 'Project 21' },
+    { path: '/cashier', component: Cashier, name: 'Cashier' },
     { path: '/debrief', component: DebriefPage, name: 'Debrief'},
 ];
 
