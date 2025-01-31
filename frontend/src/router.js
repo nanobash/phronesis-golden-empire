c52fc4525b39be33249e9178fc7c47cedcdeea39
Tornike Nanobashvili <tnanobashvili92@gmail.com>
2025-01-31 12:27:52 +0400

    Update styling

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -9,9 +9,9 @@ import DebriefPage23 from './pages/DebriefPage23.vue';
 const routes = [
     { path: '/', component: HomePage, name: 'Home' },
     { path: '/cashier', component: Cashier, name: 'Cashier' },
-    { path: '/debrief-21', component: DebriefPage21, name: 'Debrief 21'},
-    { path: '/debrief-22', component: DebriefPage22, name: 'Debrief 22'},
-    { path: '/debrief-23', component: DebriefPage23, name: 'Debrief 23'},
+    { path: '/debrief-21', component: DebriefPage21, name: 'Debrief 21' },
+    { path: '/debrief-22', component: DebriefPage22, name: 'Debrief 22' },
+    { path: '/debrief-23', component: DebriefPage23, name: 'Debrief 23' },
 ];
 
 const router = createRouter({
