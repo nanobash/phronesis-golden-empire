77ca3236e9874646f6adf38e99b7f4a518f31da5
Tornike Nanobashvili <tnanobashvili92@gmail.com>
2025-06-26 23:15:37 +0400

    Remove some routes

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,10 +2,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from './pages/HomePage.vue';
 import Cashier from './pages/Cashier.vue';
-import DebriefPage36 from "./pages/DebriefPage36.vue";
-import DebriefPage37 from "./pages/DebriefPage37.vue";
-import DebriefPage38 from "./pages/DebriefPage38.vue";
-import DebriefPage39 from "./pages/DebriefPage39.vue";
 import DebriefPage40 from "./pages/DebriefPage40.vue";
 import DebriefPage41 from "./pages/DebriefPage41.vue";
 import DebriefPage42 from "./pages/DebriefPage42.vue";
@@ -16,10 +12,6 @@ import DebriefPage45 from "./pages/DebriefPage45.vue";
 const routes = [
     { path: '/', component: HomePage, name: 'Home' },
     { path: '/cashier', component: Cashier, name: 'Cashier' },
-    { path: '/debrief-36', component: DebriefPage36, name: 'Debrief 36' },
-    { path: '/debrief-37', component: DebriefPage37, name: 'Debrief 37' },
-    { path: '/debrief-38', component: DebriefPage38, name: 'Debrief 38' },
-    { path: '/debrief-39', component: DebriefPage39, name: 'Debrief 39' },
     { path: '/debrief-40', component: DebriefPage40, name: 'Debrief 40' },
     { path: '/debrief-41', component: DebriefPage41, name: 'Debrief 41' },
     { path: '/debrief-42', component: DebriefPage42, name: 'Debrief 42' },
