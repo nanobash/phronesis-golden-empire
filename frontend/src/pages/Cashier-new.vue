f09777e54ad0006b2c1f3419174862ebd210a81b
Tornike Nanobashvili <tnanobashvili92@gmail.com>
2025-08-29 16:04:47 +0400

    Add currency to Cashier-new.vue

--- frontend/src/pages/Cashier-new.vue
+++ frontend/src/pages/Cashier-new.vue
@@ -1,10 +1,18 @@
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { BACKEND_URL } from '../config';
 
-onMounted(async () => {
+const currency = ref('USD');
+
+const switchTo = (payload) => {
+    currency.value = payload;
+    refreshState();
+};
+
+async function refreshState() {
     const response = await fetch(BACKEND_URL + "create-cashier", {
         method: "POST",
+        body: JSON.stringify({ currency: currency.value }),
         headers: {
             Accept: "application/json",
             "Content-Type": "application/json",
@@ -24,9 +32,16 @@ onMounted(async () => {
     } else {
         console.error("RebillyCashier library not loaded");
     }
+};
+
+onMounted(async () => {
+    refreshState();
 });
 </script>
 
 <template>
+    <button @click="switchTo('USD')" :disabled="currency==='USD'">USD</button>
+    <button @click="switchTo('CAD')" :disabled="currency==='CAD'">CAD</button>
+
     <div id="deposit"></div>
 </template>
