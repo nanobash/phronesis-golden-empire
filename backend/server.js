8d70c0ebf0839ca177661a40dd2a308144da2cdd
Tornike Nanobashvili <tnanobashvili92@gmail.com>
2025-08-29 16:12:23 +0400

    Update server.js with cashier.js content

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
-require('dotenv').config({ path: '.env.local' });
+import dotenv from "dotenv"
+
+dotenv.config({ path: '.env.local' });
 
 // Set environmental variables
 
@@ -10,52 +12,38 @@ const FRONTEND_ORIGIN_URL = process.env.FRONTEND_ORIGIN_URL;
 
 // -----------------------------------------------------------------------------------------
 
-const cors = require('cors');
-const express = require('express');
+import express from "express";
+import RebillyAPI from "rebilly-js-sdk";
+import cors from "cors"
+
 const app = express();
-
-const corsOptions = {
-    origin: FRONTEND_ORIGIN_URL,
-    methods: ['POST'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-};
-
 // Allow all origins
-app.use(cors(corsOptions));
+app.use(cors({}));
 
-const RebillySDK = require('rebilly-js-sdk').default;
-
-// Initalize Rebilly JS SDK with the organization and api key
-const rebilly = RebillySDK({
+const rebilly = RebillyAPI({
     organizationId: ORGANIZATION_ID,
-    sandbox: true, // Set to true for testing
+    sandbox: true,
     apiKey: API_SECRET_KEY,
+    timeout: 5000,
 });
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(express.json());
 
-app.post('/create-deposit-request', async (req, res) => {
+app.post("/create-cashier", async (req, res) => {
     try {
-        /*
-            Make a POST API request to Rebilly to create a deposit request.
-            https://www.rebilly.com/catalog/all/deposits/postdepositrequest
-        */
-        const response = await rebilly.depositRequests.create({
+        const response = await rebilly.cashiers.create({
             data: {
                 websiteId: WEBSITE_ID,
-                // Replace {{ CUSTOMER_ID }} with the ID of the customer for which
-                // a deposit request should be created.
                 customerId: CUSTOMER_ID,
-                // Replace {{ CURRENCY }} with the currency code in ISO 4217 format.
-                currency: 'USD',
-            }
+                currency: req.body.currency,
+            },
         });
-        // Extract the cashier token from the response.
-        const token = response.fields.cashierToken;
-        // Return the token.
-        res.send({ token });
+
+        const cashierToken = response.fields.cashierToken;
+
+        res.send({ cashierToken });
     } catch (error) {
-        // Log any errors that occur.
         if (error?.response?.data) {
             console.error(error.response.data);
         } else {
@@ -64,4 +52,4 @@ app.post('/create-deposit-request', async (req, res) => {
     }
 });
 
-app.listen(8080, () => console.log('Running on port 8080'));
+app.listen(8080, () => console.log("Running on port 8080"));
