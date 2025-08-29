d9e5fb819992ae226715bf0395b09089561ad03d
Tornike Nanobashvili <tnanobashvili92@gmail.com>
2025-08-29 15:00:21 +0400

    Add Cashier-new to router

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,7 +1,8 @@
 // src/router.js
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from './pages/HomePage.vue';
-import Cashier from './pages/Cashier.vue';
+// import Cashier from './pages/Cashier.vue';
+import CashierNew from './pages/Cashier-new.vue';
 import DebriefPage50 from "./pages/DebriefPage50.vue";
 import DebriefPage51 from "./pages/DebriefPage51.vue";
 import DebriefPage52 from "./pages/DebriefPage52.vue";
@@ -9,7 +10,8 @@ import DebriefPage53 from "./pages/DebriefPage53.vue";
 
 const routes = [
     { path: '/', component: HomePage, name: 'Home' },
-    { path: '/cashier', component: Cashier, name: 'Cashier' },
+    // { path: '/cashier', component: Cashier, name: 'Cashier' },
+    { path: '/cashier-new', component: CashierNew, name: 'CashierNew' },
     { path: '/debrief-50', component: DebriefPage50, name: 'Debrief 50' },
     { path: '/debrief-51', component: DebriefPage51, name: 'Debrief 51' },
     { path: '/debrief-52', component: DebriefPage52, name: 'Debrief 52' },
