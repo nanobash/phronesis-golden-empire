00a567a9c159e88f14e9bf0fa9411d2fa023df27
Tornike Nanobashvili <tnanobashvili92@gmail.com>
2025-10-03 20:11:54 +0400

    Remove old pages from menu

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,10 +3,6 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from './pages/HomePage.vue';
 // import Cashier from './pages/Cashier.vue';
 import CashierNew from './pages/Cashier-new.vue';
-import DebriefPage50 from "./pages/DebriefPage50.vue";
-import DebriefPage51 from "./pages/DebriefPage51.vue";
-import DebriefPage52 from "./pages/DebriefPage52.vue";
-import DebriefPage53 from "./pages/DebriefPage53.vue";
 import DebriefPage54 from "./pages/DebriefPage54.vue";
 import DebriefPage56 from "./pages/DebriefPage56.vue";
 import DebriefPage57 from "./pages/DebriefPage57.vue";
@@ -17,10 +13,6 @@ const routes = [
     { path: '/', component: HomePage, name: 'Home' },
     // { path: '/cashier', component: Cashier, name: 'Cashier' },
     { path: '/cashier-new', component: CashierNew, name: 'CashierNew' },
-    { path: '/debrief-50', component: DebriefPage50, name: 'Debrief 50' },
-    { path: '/debrief-51', component: DebriefPage51, name: 'Debrief 51' },
-    { path: '/debrief-52', component: DebriefPage52, name: 'Debrief 52' },
-    { path: '/debrief-53', component: DebriefPage53, name: 'Debrief 53' },
     { path: '/debrief-54', component: DebriefPage54, name: 'Debrief 54' },
     { path: '/debrief-56', component: DebriefPage56, name: 'Debrief 56' },
     { path: '/debrief-57', component: DebriefPage57, name: 'Debrief 57' },
